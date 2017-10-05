fbb3eda72af09c65469b0977625c0c4e80494bd0
Harish Mohan Raj <hraj@sapient.com>
2017-10-05 18:23:46 +0100

    example 2 added

---
+++ supervised_classification/examples/example2.py
@@ -0,0 +1,37 @@
+import re
+input_list = []
+with open('./dataset.txt','r') as f:
+  for line in f:
+    input_list.append(line.strip())
+
+N, M = input_list[0].split(" ");
+
+def feature_format(dataset, set_name):
+  temp = []
+  features, labels = [], []
+  if set_name=="train":
+    for index, item in enumerate(dataset):
+      labels.append(item.split(" ")[1])
+      features.append(item.split(" ")[2:])
+    return features, labels
+  else:
+    for index, item in enumerate(dataset):
+      item = item.split(" ")[1:]
+      features.append([re.sub(r"^(.*?):", "", y) for y in item])
+    for index, list_item in enumerate(features):
+      for pos, item in enumerate(list_item):
+        features[index][pos] = float(item)
+    return features
+
+training_features, training_labels = feature_format(input_list[1:6], 'train')
+testing_features = feature_format(input_list[7:10], 'test')
+print(testing_features)
+
+from sklearn import svm
+
+#clf = svm.SVC(kernel='rbf', C=10000.0)
+#clf.fit(training_features, training_labels)
+#pred = clf.predict(testing_features)
+#print(training_features)
+
+
