45201465291ac102ba1546437c4842344befd140
Harish Mohan Raj <hraj@sapient.com>
2017-10-05 08:07:02 +0100

    example 1 added

--- supervised_classification/examples/example1.py
+++ supervised_classification/examples/example1.py
@@ -1,12 +1,11 @@
-lines = []
-with open('./dataset.txt','r') as f:
+input_list = []
+with open('./dataset2.txt','r') as f:
   for line in f:
-    lines.append(line.strip())
+    input_list.append(line.strip())
 
-testcases = lines[0];
-number_of_admitted_students = lines[1];
-gpa = lines[2].split(" ")
-preformance_of_aptitude_tests = lines[3:]
+testcases = input_list[0];
+list_without_testcase = input_list[1:]
+list_of_test_data = [list_without_testcase[x:x+7] for x in range(0, len(list_without_testcase), 7)]
 
 def sort_and_return_index(list):
   student_dict = {}
@@ -15,20 +14,20 @@ def sort_and_return_index(list):
   return [value for (key, value) in sorted(student_dict.items(), reverse=True)]
 
 def get_sorted_test_list(list_item):
-  res = [];
+  sorted_test_list = [];
   for item in list_item:
     splitted_list = item.split(" ")
-    res.append(sort_and_return_index(list(map(int, splitted_list))))
-  return res
+    sorted_test_list.append(sort_and_return_index(list(map(float, splitted_list))))
+  return sorted_test_list
 
 def sort_and_return_length(gpa_sorted_by_rank, test_rank):
   return len([i for i, j in zip(gpa_sorted_by_rank, test_rank) if i == j])
 
 def get_most_relevalt_test(gpa_sorted_by_rank, aptitude_tests_sorted_by_rank):
-  result_len = []
+  most_relevant_test = []
   for test_rank in aptitude_tests_sorted_by_rank:
-    result_len.append(sort_and_return_length(gpa_sorted_by_rank, test_rank))
-  return result_len
+    most_relevant_test.append(sort_and_return_length(gpa_sorted_by_rank, test_rank))
+  return most_relevant_test
 
 def best_aptitude_test_finder():
   gpa_sorted_by_rank = sort_and_return_index(gpa)
@@ -36,4 +35,9 @@ def best_aptitude_test_finder():
   relevant_apptitude_test = get_most_relevalt_test(gpa_sorted_by_rank, aptitude_tests_sorted_by_rank)
   return relevant_apptitude_test.index(max(relevant_apptitude_test))+1
 
-print(best_aptitude_test_finder())
+for index, item in enumerate(list_of_test_data):
+  #number_of_admitted_students = item[1];
+  gpa = item[1].split(" ")
+  preformance_of_aptitude_tests = item[2:]
+  print(best_aptitude_test_finder());
+  #print(item[2:])
